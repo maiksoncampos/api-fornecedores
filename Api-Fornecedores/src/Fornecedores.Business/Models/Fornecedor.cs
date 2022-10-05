6079960eab0d544bd82de4389c251de19a43b836
maiksoncampos <maikson.campos@gmail.com>
2022-10-05 17:58:45 -0300

    Estrutura inicial do projeto

---
+++ Api-Fornecedores/src/Fornecedores.Business/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace DevIO.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<Endereco> Enderecos { get; set; }
+    }
+}
